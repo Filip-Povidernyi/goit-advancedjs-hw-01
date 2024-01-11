db2296ea095053f1b6918a777a54d6d268236284
Filip-Povidernyi <p.poviderniy@gmail.com>
2024-01-11 11:42:49 +0200

    home_work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const markupList = galleryItems.map(({ preview, original, description }) => {
+    return `<li>
+        <a class="gallery__link" href=${original}>
+            <img
+            class='gallery__image'
+            src="${preview}" 
+            data-source="${original}" 
+            alt="${description}"
+            />
+        </a>
+    </li>`;
+});
+galleryList.insertAdjacentHTML("beforeend", markupList.join(''));
+const lightBox = new SimpleLightbox('.gallery a',
+    {
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
 
+const LS_KEY = "feedback-form-state";
+
+let form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(LS_KEY)) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInputData(event) {
+    dataForm = { email: email.value, message: message.value };
+    localStorage.setItem(LS_KEY, JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+    if (dataForm) {
+        email.value = dataForm.email || '';
+        message.value = dataForm.message || '';
+    }
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log({ email: email.value, message: message.value });
+
+    // if (email.value === '' || message.value === '') {
+    //     return alert('Please fill in all the fields!');
+    // }
+
+    localStorage.removeItem(LS_KEY);
+    event.currentTarget.reset();
+    dataForm = {};
+}
